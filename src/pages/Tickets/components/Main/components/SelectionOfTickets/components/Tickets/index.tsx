b464fd87bb782f65bc71ea72ec38b01019c73713
AlexSudarev <sudarevwork@gmail.com>
2023-12-14 00:01:19 +0300

    a

--- src/pages/Tickets/components/Main/components/SelectionOfTickets/components/Tickets/index.tsx
+++ src/pages/Tickets/components/Main/components/SelectionOfTickets/components/Tickets/index.tsx
@@ -8,24 +8,47 @@ const Tickets = ({ dataTickets }: { dataTickets: any }) => {
   return (
     <Layout>
       <ColLeft>
-        <div>
+        <div className={styles.sortWrapper}>
           <div>Дата поездки</div>
           <div>Дата возвращения</div>
         </div>
-        <div>
-          <p>Последние билеты</p>
-        </div>
       </ColLeft>
       <ColRight>
-        <header>
-          <p>найдено {dataTickets.items.length}</p>
-          <div>
-            <p>
+        <header className={styles.header}>
+          <p className={styles.headerCount}>
+            найдено {dataTickets.items.length}
+          </p>
+          <div className={styles.headerSort}>
+            <p className={styles.headerSelectWrapper}>
               сортировать по: <span>времени</span>
             </p>
-            <p>показывать по 5</p>
+            <p className={styles.headerSortCount}>показывать по 5</p>
           </div>
         </header>
+        {dataTickets && (
+          <ul className={styles.tickets}>
+            {dataTickets.items.map((item: any, id: number) => {
+              return (
+                <li key={id} className={styles.ticket}>
+                  <div className={styles.ticketLeft}>
+                    <p>{item.departure.train.name}</p>
+                    <div>
+                      <p>
+                        {item.departure.from.city.name} «
+                        {item.departure.from.railway_station_name}»{' '}
+                      </p>
+                      <p>
+                        {item.departure.to.city.name} «
+                        {item.departure.to.railway_station_name}»
+                      </p>
+                    </div>
+                  </div>
+                  <div></div>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </ColRight>
     </Layout>
   );
